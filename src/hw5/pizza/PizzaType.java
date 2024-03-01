9b10c85df415dde084b6eb02d2cb1ebc13b02cbe
hargu <hargu@10.7.79.204>
2024-03-01 16:26:01 -0600

    Added New Enum: PizzaType
    Not fully implemented yet. Includes only the possible Pizza types. Some
    methods and an attribute needs added.

---
+++ src/hw5/pizza/PizzaType.java
@@ -0,0 +1,12 @@
+package hw5.pizza;
+
+/**
+ * TODO: implement a private toppingPrice(double) atrribute, a constructor and getter
+ * @author hargu
+ */
+public enum PizzaType {
+	HAWAIIAN,
+	MARGHERITA,
+	SUPREME,
+	VEGETARIAN
+}
