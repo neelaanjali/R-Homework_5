22f7624558e285f59b2885a1cfcfe51ba0a29fd6
riannaellis <riannaellis@null-bea87974e3c4.dhcp.missouri.edu>
2024-03-05 10:51:58 -0600

    Declared CookingStrategy subclasses
    Declared BrickOvenCookingStrategy, ConventionalOvenCookingStrategy, and
    MicrowaveCookingStrategy subclasses. Declared but did not develop the
    overridden cook method.

---
+++ src/hw5/pizza/strategies/BrickOvenCookingStrategy.java
@@ -0,0 +1,14 @@
+package hw5.pizza.strategies;
+
+import hw5.pizza.AbstractPizza;
+import hw5.pizza.ICookingStrategy;
+
+public class BrickOvenCookingStrategy implements ICookingStrategy {
+
+	@Override
+	public boolean cook(AbstractPizza pizza) {
+		
+		return false;
+	}
+
+}
---
+++ src/hw5/pizza/strategies/ConventionalOvenCookingStrategy.java
@@ -0,0 +1,14 @@
+package hw5.pizza.strategies;
+
+import hw5.pizza.AbstractPizza;
+import hw5.pizza.ICookingStrategy;
+
+public class ConventionalOvenCookingStrategy implements ICookingStrategy  {
+
+	@Override
+	public boolean cook(AbstractPizza pizza) {
+		
+		return false;
+	}
+
+}
