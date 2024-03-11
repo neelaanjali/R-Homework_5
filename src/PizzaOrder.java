fb22d4163e53b9a7c680e5934b27ad010e5f656e
Neela Hanumanthiah <neelahanumanthiah@mu-584783.dhcp.missouri.edu>
2024-03-10 22:27:30 -0500

    imported import hw5.pizza.AbstractPizza; import hw5.pizza.ICookingStrategy; import hw5.pizza.PizzaCookingFactory;
    and also implemented the method public AbstractPizza getPizzaByOrderID

--- src/PizzaOrder.java
+++ src/PizzaOrder.java
@@ -1,6 +1,30 @@
 import java.util.List;
+
+import hw5.pizza.AbstractPizza;
+import hw5.pizza.ICookingStrategy;
+import hw5.pizza.PizzaCookingFactory;
+
 import java.util.ArrayList;
 
 public class PizzaOrder {
-    
-}
+	   private PizzaCookingFactory pizzaFactory;
+	    private ICookingStrategy cookingStrategy;
+	    private List<AbstractPizza> pizzaOrderList;
+	    
+	    public PizzaOrder() {
+	        this.pizzaFactory = new PizzaCookingFactory();
+	        this.pizzaOrderList = new ArrayList<>();
+	    }
+	    
+	    
+	    public AbstractPizza getPizzaByOrderID(int orderID) {
+	        for (AbstractPizza pizza : pizzaOrderList) {
+	            if (pizza.getPizzaOrderID() == orderID) {
+	                return pizza;
+	            }
+	        }
+	        return null;
+	    }
+
+ }
+
