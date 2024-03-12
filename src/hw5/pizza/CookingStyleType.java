b03e8a97608aaf52c589f5d9d9f14881745a60e1
riannaellis <riannaellis@riannas-mbp.dhcp.missouri.edu>
2024-03-12 17:24:21 -0500

    Added JavaDoc for CookingStyleType and ICookingStrategy
    Added JavaDoc for CookingStyleType enum and ICookingStrategy interface.

--- src/hw5/pizza/CookingStyleType.java
+++ src/hw5/pizza/CookingStyleType.java
@@ -1,5 +1,11 @@
 package hw5.pizza;
 
+
+/**
+ * Enum representing different cooking styles
+ * @author riannaellis
+ */
+
 public enum CookingStyleType {
 	MICROWAVE,
 	CONVENTIONAL_OVEN,
--- src/hw5/pizza/ICookingStrategy.java
+++ src/hw5/pizza/ICookingStrategy.java
@@ -1,5 +1,17 @@
 package hw5.pizza;
 
+/**
+ * Interface with a method to cook a pizza
+ * @author riannaellis
+ */
+
+/**
+ * Cooks a pizza using a cooking strategy
+ * 
+ * @param pizza The pizza that will be cooked
+ * @return returns true if the pizza is cooked, returns false if there is an error
+ * @author riannaellis
+ */
 public interface ICookingStrategy {
 
 	public boolean cook(AbstractPizza pizza);
